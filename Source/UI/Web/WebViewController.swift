98a52aa2709f0b7cac612bda963bbbc9121a1a32
Marius Kurgonas <marius.kurgonas@getivy.de>
2023-05-16 09:22:19 +0100

    chore: cleanup

--- Source/UI/Web/WebViewController.swift
+++ Source/UI/Web/WebViewController.swift
@@ -44,28 +44,3 @@ class WebViewController: UIViewController {
         #endif
     }
 }
-
-extension WebViewController: WKScriptMessageHandler {
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        guard let dictionary = message.body as? [String: AnyObject],
-              let source = dictionary["source"] as? String,
-              let sourceEnum = WebMessageSource(rawValue: source),
-              let type = dictionary["type"] as? String,
-              let typeEnum = WebMessageType(rawValue: type),
-              let refId = dictionary["referenceId"] as? String, let dataId = dictionary["dataId"] as? String,
-              let value = dictionary["value"] as? String,
-              let outcome = WebMessageOutcome(rawValue: value) else {
-            return
-        }
-
-        let result = WebResult(
-            dataId: dataId,
-            referenceId: refId,
-            source: sourceEnum,
-            type: typeEnum,
-            value: outcome
-        )
-
-        router.handleWebResult(result: result)
-    }
-}
