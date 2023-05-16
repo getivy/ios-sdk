ecde1ef6114a37aa1aaf004cb83237546cf7274d
Marius Kurgonas <marius.kurgonas@getivy.de>
2023-05-16 09:27:59 +0100

    chore: make dataid and referenceid optional

--- Source/UI/Web/WebViewController.swift
+++ Source/UI/Web/WebViewController.swift
@@ -11,10 +11,10 @@ class WebViewController: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+
         router.closeButton.isHidden = false
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -39,30 +39,5 @@ class WebViewController: UIViewController {
             webView.isInspectable = true
         }
         #endif
-    }
-}
-
-extension WebViewController: WKScriptMessageHandler {
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        guard let dictionary = message.body as? [String: AnyObject],
-              let source = dictionary["source"] as? String,
-              let sourceEnum = WebMessageSource(rawValue: source),
-              let type = dictionary["type"] as? String,
-              let typeEnum = WebMessageType(rawValue: type),
-              let refId = dictionary["referenceId"] as? String, let dataId = dictionary["dataId"] as? String,
-              let value = dictionary["value"] as? String,
-              let outcome = WebMessageOutcome(rawValue: value) else {
-            return
-        }
-
-        let result = WebResult(
-            dataId: dataId,
-            referenceId: refId,
-            source: sourceEnum,
-            type: typeEnum,
-            value: outcome
-        )
-
-        router.handleWebResult(result: result)
     }
 }
