0abfd1c7ac554f127e1abc29f3a2bf7fbbac9152
Marius Kurgonas <marius.kurgonas@getivy.de>
2023-05-23 22:49:01 +0100

    chore: remove dev environment

--- Source/Api/Environment.swift
+++ Source/Api/Environment.swift
@@ -4,7 +4,6 @@ import Foundation
 public enum Environment: Int {
     case production
     case sandbox
-    case development
 
     var url: String {
         switch self {
@@ -12,8 +11,6 @@ public enum Environment: Int {
             return "https://api.getivy.de"
         case .sandbox:
             return "https://api.sand.getivy.de"
-        case .development:
-            return "https://api.dev.getivy.de"
         }
     }
 
@@ -23,8 +20,6 @@ public enum Environment: Int {
             return "https://account-check.getivy.de"
         case .sandbox:
             return "https://account-check.sand.getivy.de"
-        case .development:
-            return "https://account-check.dev.getivy.de"
         }
     }
 }
